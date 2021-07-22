42e12d8a6fecda57ca6d2ce3be8d03f63fff52bd
Matthew Caesario Jawak <63052871+mthwc99@users.noreply.github.com>
2021-07-22 11:57:26 +0700

    first commit

---
+++ my portfolio b4 (3)/js/about.js
@@ -0,0 +1,9 @@
+// parralax
+$(window).on('load', function(){
+	$('.navbaratas').addClass('navbaratasShow')
+	$('.navbarbawah').addClass('navbarbawahShow')
+	$('.h3Kiri').addClass('h3KiriShow')
+	$('.h3Kanan').addClass('h3KananShow')
+	$('.h3Kiri1').addClass('h3Kiri1Show')
+	$('.h3Kanan1').addClass('h3Kanan1Show')
+});
---
+++ my portfolio b4 (3)/js/index.js
@@ -0,0 +1,7 @@
+// parralax
+$(window).on('load', function(){
+	$('.navbaratas').addClass('navbaratasShow')
+	$('.navbarbawah').addClass('navbarbawahShow')
+	$('.pbawah').addClass('pbawahShow')
+	$('.h1Main').addClass('h1MainShow')
+});
